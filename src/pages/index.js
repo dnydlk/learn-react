37ca038e5a36db6e18361b963b41dda38bd5a03b
Duolikun-Daniya <daniya.d@northeastern.edu>
2024-02-20 18:03:43 -0500

    Complete quizzes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,11 @@
-// import Head from 'next/head'
-// import Image from 'next/image'
-// import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Profile from './components/profile'
+import styles from "@/styles/Home.module.css"
+// import Component from "./components/artistsArr"
+import Component from "./qcomps/arrObj"
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+      <Component />
     </div>
   )
 }
--- src/pages/qcomps/shoppingCart.js
+++ src/pages/qcomps/shoppingCart.js
@@ -1,33 +1,29 @@
-import { useState } from 'react';
+import { useState } from "react"
 
 export default function RequestTracker() {
-  const [pending, setPending] = useState(0);
-  const [completed, setCompleted] = useState(0);
-
+  const [pending, setPending] = useState(0)
+  const [completed, setCompleted] = useState(0)
   async function handleClick() {
-    setPending(pending + 1);
-    await delay(3000);
-    setPending(pending - 1);
-    setCompleted(completed + 1);
+    // setPending(pending + 1)
+    setPending((currentPending) => currentPending + 1)
+    await delay(1000)
+    // setPending(pending - 1)
+    setPending((currentPending) => currentPending - 1)
+    // setCompleted(completed + 1)
+    setCompleted((currentCompleted) => currentCompleted + 1)
   }
 
   return (
     <>
-      <h3>
-        Pending: {pending}
-      </h3>
-      <h3>
-        Completed: {completed}
-      </h3>
-      <button onClick={handleClick}>
-        Buy
-      </button>
+      <h3>Pending: {pending}</h3>
+      <h3>Completed: {completed}</h3>
+      <button onClick={handleClick}>Buy</button>
     </>
-  );
+  )
 }
 
 function delay(ms) {
-  return new Promise(resolve => {
-    setTimeout(resolve, ms);
-  });
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
 }
